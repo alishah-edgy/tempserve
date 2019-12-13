f3c764bcec60c18ecbf0b54d7c25e37f6140cca4
Ali <35536928+alishah-edgy@users.noreply.github.com>
2019-12-13 13:28:34 +0500

    Update index.js

--- plugins/publishing-plugin2/index.js
+++ plugins/publishing-plugin2/index.js
@@ -4,9 +4,9 @@ class darwin {
     this._INK = _INK;
     this.ipcRenderer = ipcRenderer;
     console.log("Darwin 2.0", _INK, ipcRenderer);
+    _INK["menu-item"]({icon: "abc", name: "test"});
     this.clickEvent = function () {
       console.log('Darwin 2.0 activated');
-      _INK["menu-item"]({icon: "abc", name: "test"});
     }
   }
 }
