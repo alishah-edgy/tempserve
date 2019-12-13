056d9e142ee5988297791fd2aee24de069a82e2d
Ali <35536928+alishah-edgy@users.noreply.github.com>
2019-12-13 13:33:02 +0500

    minor

--- plugins/publishing-plugin2/index.js
+++ plugins/publishing-plugin2/index.js
@@ -4,7 +4,7 @@ class darwin {
     this._INK = _INK;
     this.ipcRenderer = ipcRenderer;
     console.log("Darwin 2.0", _INK, ipcRenderer);
-    _INK["menu-item"]({icon: "abc", name: "test"});
+    _INK["menu-item"]({icon: "abc", name: "Test Plugin", classId: 'darwin-menu-icon'});
     this.clickEvent = function () {
       console.log('Darwin 2.0 activated');
     }
