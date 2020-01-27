faa781558a39ae33acf362eb2a8ba25f70168756
Syed Faris Ahmed <60087813+FarisEdgy@users.noreply.github.com>
2020-01-27 17:49:47 +0500

    testing

--- plugins/wordpress-plugin/index.js
+++ plugins/wordpress-plugin/index.js
@@ -1,12 +1,28 @@
+import { editor, exposedAPI } from 'EditorAPI';
 
 class darwin {
   stage = 'start';
   constructor(_INK, ipcRenderer) {
     this._INK = _INK;
     this.stage = 'start';
+    console.log(editor, exposedAPI);
+    editor("DownloadPlugins", { data: "some dummy data" }).then(res => {
+      const { pluginData, error } = res;
+      if (error) {
+        return console.log(error);
+      }
+      if (!insertedPlugins.includes(pluginData.id)) {
+        const plugin = require(`${tempPath}/${downloadLink.name}/${pluginData.main}`);
+        var thisPlugin = new plugin(_INK);
+        pluginData.clickEvent = thisPlugin.clickEvent;
+        addPluginCB(pluginData);
+        insertedPlugins.push(pluginData.id);
+      }
+    });
     // open keyInput Form
     // addInputField('Secret Key','secretKey', styles);
     // addButton('Verify Key', this.onKeySubmit, styles);
+
     this.ipcRenderer = ipcRenderer;
     console.log("Darwin 2.0", _INK, ipcRenderer);
     _INK["menu-item"]({ icon: "abc", name: "Test Plugin", classId: 'darwin-menu-icon' });
