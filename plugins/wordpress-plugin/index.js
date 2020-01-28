47e7b58c9980eac88fb85edabdb2c0d82467c9bd
Syed Faris Ahmed <60087813+FarisEdgy@users.noreply.github.com>
2020-01-28 16:17:19 +0500

    TESTING

--- plugins/wordpress-plugin/index.js
+++ plugins/wordpress-plugin/index.js
@@ -7,34 +7,46 @@ class darwin {
   constructor(_INK, ipcRenderer) {
     this._INK = _INK;
     this.stage = 'start';
-
+    this.inputFields = [
+      {
+        name: "wordpress key",
+        type: "input"
+      },
+      {
+        name: "jwt token",
+        type: "input"
+      },
+      {
+        name: "publish",
+        type: "btn"
+      },
+    ];
+    this.insertInputFields();
     // open keyInput Form
     // addInputField('Secret Key','secretKey', styles);
     // addButton('Verify Key', this.onKeySubmit, styles);
-
     this.ipcRenderer = ipcRenderer;
     console.log("Wordpress Plugin", _INK, ipcRenderer);
-    document.write("You have been hacked!!!!");
-    this.useCommand(_INK.commands.addInputField, {
-      "name": "username",
-      "type": "input"
-    }).then(res => {
-      const { error } = res;
-      if (error) {
-        return console.log(error);
-      }
-    }, err => {
-      console.log(err);
-    });
-
     // _INK["menu-item"]({ icon: "abc", name: "Test Plugin", classId: 'darwin-menu-icon' });
     this.clickEvent = function () {
       console.log('Darwin 2.0 activated');
     }
   }
 
+  insertInputFields() {
+    this.inputFields.map(inputData => {
+      this.useCommand(_INK.commands.addInputField, { pluginId: _INK.id, inputData }).then(res => {
+        const { error } = res;
+        if (error) {
+          return console.log(error);
+        }
+      }, err => {
+        console.log(err);
+      })
+    });
+  }
+
   useCommand(event, payload) { // payload must be an object
-    console.log("perform plugin ran:" + payload, event);
     return new Promise((resolve) => {
       payload._workID = workID++;
       this.ipcRenderer.send('plugin-perform', event, payload); // goes to plugin-window.js
