d5cc621c6f565a0365f9674e1d85f665f4d4af26
Syed Faris Ahmed <60087813+FarisEdgy@users.noreply.github.com>
2020-01-28 11:04:35 +0500

    testing

--- plugins/wordpress-plugin/index.js
+++ plugins/wordpress-plugin/index.js
@@ -8,7 +8,7 @@ class darwin {
     // addButton('Verify Key', this.onKeySubmit, styles);
 
     this.ipcRenderer = ipcRenderer;
-    console.log("Darwin 2.0", _INK, ipcRenderer);
+    console.log("Wordpress Plugin", _INK, ipcRenderer);
     _INK["menu-item"]({ icon: "abc", name: "Test Plugin", classId: 'darwin-menu-icon' });
     this.clickEvent = function () {
       console.log('Darwin 2.0 activated');
